c8ee07083d2d5fbdbf005df6e8cf5c1a0f4e63c7
Vladimir Reymer <vladimireymer@gmail.com>
2024-05-27 20:58:00 +0700

    Main logger functionality

--- server/logger.py
+++ server/logger.py
@@ -3,4 +3,17 @@ import logging
 
 class Logger:
     def __init__(self) -> None:
-        pass
+        self.log = logging.getLogger('__logger__')
+        self.log.setLevel(logging.DEBUG)
+
+        # Create a file for logger handling
+        handler = logging.FileHandler('logs.log', mode='a', encoding='utf-8')
+        # set a format to log message
+        formatter = logging.Formatter(
+            '%(asctime)s\t%(levelname)s\t%(message)s', '%d-%m-%Y %H:%M:%S')
+
+        handler.setFormatter(formatter)  # Add formatter to handler
+        self.log.addHandler(handler)  # Add handler to logger
+
+
+logger = Logger()
