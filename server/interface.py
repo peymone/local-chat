7ab25556eec7d62f56d67c62a90f7d8d87e6c6b5
Vladimir Reymer <vladimireymer@gmail.com>
2024-05-28 07:53:21 +0700

    Comments for interface

--- server/interface.py
+++ server/interface.py
@@ -8,6 +8,8 @@ from pyfiglet import figlet_format
 
 
 class Interface:
+    """Class for custom UI - colors, logo, etc."""
+
     def __init__(self) -> None:
         self.tFormat = "%H:%M:%S"
         self.__theme = Theme({
@@ -23,35 +25,41 @@ class Interface:
         self.console = Console(theme=self.__theme)
 
     def enter(self, prompt: str, style='system') -> str:
+        """Custom input method with prompt and color"""
+
         return self.console.input(f"[{style}]{prompt}[/{style}]")
 
     def show(self, message: str, sender: str = 'system', style: str = 'system') -> None:
+        """Custom print method with color and time"""
+
         now = datetime.now().strftime(self.tFormat)
 
         match sender:
             # System can be warning message or just information
             case 'system':
-                # print('\n')
                 self.console.print(f"[{style}]{now} {message}[/{style}]")
-                # print('\n')
             case _:
                 msg = f"[{style}]{now} {sender}:[/{style}]  {message}"
-                # print('\n')
                 self.console.print(msg)
-                # print('\n')
 
     def show_logo(self, logo_text: str = 'local chat') -> None:
+        """Show logo with pyfiglet lib"""
+
         logo = figlet_format(logo_text, font='larry3d')
         self.console.print(logo, style='logo')
         self.console.print('(c) by Cassidy Bell', style='license')
 
     def start_prompt(self) -> bool:
+        """Starting choice for turning on the server"""
+
         confirm = Confirm(console=self.console)
         confirmation = confirm.ask("Do you want to start server?")
         self.clear_screen()
         return confirmation
 
     def clear_screen(self):
+        """Clear the screen of characters"""
+
         ui.console.clear()
 
 
